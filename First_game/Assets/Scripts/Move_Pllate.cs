ca82292754acd919e35e022d53e7a33cc8737517
TWorkerFrog <lechar2d2@gmail.com>
2024-01-23 18:41:37 +0500

    Subway_bez_serfa

---
+++ First_game/Assets/Scripts/Move_Pllate.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed = 2f;
+    private Vector3 moveVector = new Vector3(0,0,100);
+    public GameObject plate2 = new GameObject();
+    // Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    // Update is called once per frame
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        moveVector.z = -2;
+        rb.MovePosition(rb.position + moveVector * speed * Time.deltaTime);
+        if (transform.position.z <= -4)
+        {
+            Vector3 mV = new Vector3(0, 0, 24);
+            //rb.MovePosition(mV);
+            plate2.transform.position = new Vector3(0, 0, transform.position.z + 60);
+
+
+        }
+       
+    }
+}
